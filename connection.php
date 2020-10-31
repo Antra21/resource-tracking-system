a936c43c35970be3e17a9562dd71f6c238a0829d
ANTRA <42299125+Antra21@users.noreply.github.com>
2020-10-31 18:03:49 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ if(!isset($_SESSION["login_status"]) || !isset($_SESSION["login_admin_id"]))
 	header("location:/login.php");
 	exit();
 }
-	$con=mysqli_connect("sql12.freemysqlhosting.net","sql12356786","2jYxUWS41A","sql12356786");
+	$con=mysqli_connect("sql12.freemysqlhosting.net","sql12373641","zk1hbWN4RN","sql12373641");
 	$admin_id = $_SESSION["login_admin_id"];
 	$query = "SELECT * FROM employee WHERE email='$admin_id'";
 	$res = mysqli_query( $con, $query );
